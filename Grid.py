b406399d7a5e8745f1e7be94010370dd135596c9
Sirinya Richardson <sirinya@lemonbrain.ch>
2023-03-06 20:05:21 +0100

    Style the sudoku grid. Add lines and headings for columns and rows, close #50

--- Grid.py
+++ Grid.py
@@ -1,3 +1,4 @@
+# from Label import *
 from pprint import pprint
 
 
@@ -7,11 +8,42 @@ class Grid:
         self.create_empy_board()
         # self.__reset_board()
 
+    def __str__(self):
+        grid_string = "    "
+        index = 1
+        for letter in range(ord('a'), ord('j')):
+            grid_string += "  " + chr(letter) + " "
+            if index % 3 == 0 and index != 9:
+                grid_string += "   "
+            index += 1
+
+        grid_string += "\n " + "_____" * 9 + "\n"
+
+        for i in range(9):
+            grid_string += str(i + 1) + " | "
+
+            if i % 3 == 0 and i != 0:
+                print("-" * 30)
+
+            for j in range(9):
+                if j % 3 == 0 and j != 0:
+                    grid_string += " | "
+                if j == 8:
+                    grid_string += "  " + str(self.grid[i][j]) + " "
+                else:
+                    grid_string += "  " + str(self.grid[i][j]) + " "
+            grid_string += "\n"
+
+            grid_string += " " + "_____" * 9 + "\n"
+        return f"{grid_string}"
+
     def create_empy_board(self):
         """
         Initialises with an empty board
         """
         # self.grid = [[(chr(x), y) for x in range(ord('a'), ord('j') + 1)] for y in range(1, 10)]
         self.grid = [[0 for x in range(ord('a'), ord('j') + 1)] for y in range(1, 10)]
+        return self.grid
 
-        pprint(self.grid)
+
+
--- run.py
+++ run.py
@@ -4,6 +4,9 @@ from pprint import pprint
 
 def main():
     grid = Grid()
+    print(grid)
+    # grid.create_empy_board()
+    # pprint(grid.create_styled_board())
 
 
 main()
