f62fd323f2c43cfa3e883dcee03dc72f0d947151
Sirinya Richardson <sirinya@lemonbrain.ch>
2023-03-10 16:07:55 +0100

    Third attempt to revert. Fail revert. Manual change from comparison

--- Grid.py
+++ Grid.py
@@ -42,7 +42,7 @@ class Grid:
         """
         Initialises with an empty board
         """
-        self.grid = [[0 for x in range(ord('a'), ord('j') + 1)] for y in range(1, 10)]
+        self.grid = [[0 for x in range(9)] for y in range(9)]
         return self.grid
 
     def create_completed_game(self):
@@ -67,5 +67,68 @@ class Grid:
                     for column in range(range_values[0], range_values[1]):
                         random_num = random.choice(sudoku_numbers)
                         self.grid[row][column] = random_num
-                        print(self.grid)
                         sudoku_numbers.remove(random_num)
+
+        self.fill_in_zeroes()
+
+    def find_zeroes(self):
+        for row in range(len(self.grid)):
+            for column in range(len(self.grid[row])):
+                if self.grid[row][column] == 0:
+                    return row, column
+
+        return False
+
+    def fill_in_zeroes(self):
+        """
+        Fills in the grid.
+        Replaces the cells that contain 0 with a random number
+        """
+        for row in range(len(self.grid)):
+            for column in range(len(self.grid[row])):
+                if self.grid[row][column] == 0:
+                    random_number = random.randint(1, 9)
+                    cell = self.find_zeroes()
+                    # print(self.find_zeroes()[0])
+                    if self.check_cell(random_number, cell):
+                        self.grid[cell[0]][cell[1]] = random_number
+
+        # print(type(self.grid[row]))
+        # print(self.grid[row].index(random_number))
+        # if random_number in self.grid[row]:
+        #     print(f"random number is already in {self.grid[row]}, {random_number}")
+        # elif random_number in self.grid[column]:
+        #     # return False
+        #     print(f"random number is already in {self.grid[column]}, {random_number}")
+        # else:
+        #     # self.grid[row][column] = random_number
+        #     print(f"random number is not in {self.grid[column]} or {self.grid[row]}, {random_number}")
+
+        # try:
+        #     self.grid[row].index(random_number)
+        #     return True
+        # except ValueError as e:
+        #     print(f"random: {random_number}")
+
+        # pprint(f"column: {self.grid[row]} {self.grid[column]}")
+
+    def check_cell(self, number, cell):
+        """
+        Checks if the cell contains a zero and is not already in the row or column
+        cell is a tuple, first in tuple is row, second in tuple is column
+        """
+        if number in self.grid[cell[0]]:
+            return False
+
+        numbers_in_column = [self.grid[row][cell[1]] for row in range(len(self.grid))]
+        if number in numbers_in_column:
+            return False
+
+        three_by_three = [self.grid[row + ((cell[0] // 3) * 3)][column + ((cell[1] // 3) * 3)] for row in range(3) for
+                          column in range(3)]
+
+        if number in three_by_three:
+            return False
+
+        return True
+        # print(f"three by three: {three_by_three}")
