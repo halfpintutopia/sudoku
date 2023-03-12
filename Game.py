e7100d95194001ba7d96c38adb0e1d62966a2d93
Sirinya Richardson <sirinya@lemonbrain.ch>
2023-03-12 19:48:26 +0100

    Testing to see if ANSI escape code positioning text on the x axis

--- Game.py
+++ Game.py
@@ -26,16 +26,15 @@ class Game:
         """
         Show options for the user to enter their choice
         """
-        print(f"\x1b[10;1H1. Play")
-        print(f"\x1b[11;1H2. Enter your own puzzle")
-        print(f"\x1b[12;1H3. Solve a puzzle")
-        print(f"\x1b[13;1H4. Instructions")
+        print(f"\x1b[10;50H1. Play")
+        print(f"\x1b[11;50H2. Enter your own puzzle")
+        print(f"\x1b[12;50H3. Solve a puzzle")
+        print(f"\x1b[13;50H4. Instructions")
 
-        choice = input(f"\x1b[15:1HEnter a number:\n")
-        # print(f"\x1b[15;1H{' ' * 20}")
-        print(f"\x1b[2J") # Erase entire screen
-        print(f"\x1b[H")
-        print(f"\x1b[15:1H{choice}", end="\r")
+        choice = input(f"\x1b[15:50HEnter a number:\n")
+        print(f"\x1b[2J")  # Erase entire screen
+        print(f"\x1b[H")  # Puts cursor back to home position
+        print(f"\x1b[15:50H{choice}", end="\r")
         # print("1. Play")
         # print("2. Enter your own puzzle")
         # print("3. Solve a puzzle")
