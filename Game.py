3f729608b866b6647c33f6cf2b70c75d40ce8e4d
Sirinya Richardson <sirinya@lemonbrain.ch>
2023-03-13 16:04:37 +0100

    Initial tidy up file for Game and add options. Add helper functions for clear_line, write_line and on, which combines both helper functions

--- Game.py
+++ Game.py
@@ -31,25 +31,39 @@ class Game:
         Show options for the user to enter their choice
         """
         # self.set_title()
-        print(f"\x1b[10;55H1. Play")
-        print(f"\x1b[11;55H2. Enter your own puzzle")
-        print(f"\x1b[12;55H3. Solve a puzzle")
-        print(f"\x1b[13;55H4. Instructions")
+        self.on(10, 55, '1. Play')
+        self.on(11, 55, '2. Enter your own puzzle')
+        self.on(12, 55, '3. Solve a puzzle')
+        self.on(13, 55, '4. Instructions')
 
-        print(f"\x1b[10;55H{' ' * 25}")
-        print(f"\x1b[10;55H1. Test-1")
+        choice = 0
 
-        print(f"\x1b[11;55H{' ' * 25}")
-        print(f"\x1b[11;55H2. Test-2")
+        while True:
+            try:
+                self.clear_line(16, 55)
+                print(f"\x1b[15;55H")
+                choice = int(input(f"\x1b[15;55HEnter a number: "))
+            except ValueError as e:
+                print(f"Please choose one of the options. 1, 2, 3, or 4")
+                continue
+            else:
+                break
 
-        print(f"\x1b[12;55H{' ' * 25}")
-        print(f"\x1b[12;55H3. Test-3")
+        match choice:
+            case 1:
+                print('You selected 1')
+            case 2:
+                print('You selected 2')
+            case 3:
+                print('You selected 3')
+            case 4:
+                print('You selected 4')
 
-        print(f"\x1b[13;55H{' ' * 25}")
-        print(f"\x1b[13;55H4. Test-4")
 
-        print(f"\x1b[15;55H")
-        choice = input(f"\x1b[15;55HEnter a number: ")
+
+        # print(f"\x1b[15;55H")
+        # choice = input(f"\x1b[15;55HEnter a number: ")
+
         # print(f"\x1b[2J")  # Erase entire screen
         # print(f"\x1b[H")  # Puts cursor back to home position
         # print(f"\x1b[15;50H{choice}", end="\r")
@@ -64,3 +78,24 @@ class Game:
         # if self.validate_difficulty_input(difficulty_level):
         #     print(int(difficulty_level))
         #     break
+
+    def on(self, row, col, string, num_of_blanks=25):
+        """
+        Combine clear_line and write_line
+        """
+        self.clear_line(row, col, num_of_blanks)
+        self.write_line(row, col, string)
+
+    def clear_line(self, row, col, num_of_blanks=25):
+        """
+        Before writing a new line, clears the line by over-writing with blank spaces
+        This is used for the right side of the screen
+        Set number of blank spaces to be more or less dependent on the number of character needed to be overwritten
+        """
+        print(f"\x1b[{row};{col}H{' ' * num_of_blanks}")
+
+    def write_line(self, row, col, string):
+        """
+        Add text at a particular row and column of the screen
+        """
+        print(f"\x1b[{row};{col}H{string}")
